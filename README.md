93adbc0cb9f0b810e4e57929c7d0ef51e860e8a0
Toni Melisma <toni.melisma@iki.fi>
2020-10-15 00:29:25 +0300

    Finalizing

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ with HEIF support. To use the packages:
 `sudo add-apt-repository ppa:tonimelisma/ppa`
 
 `sudo apt install libvips42`
+
+Usage
+
+`docker build -f Dockerfile-20.10 -t build-ubuntu-20.10`
+
+`docker run --rm -it -v /tmp/volume:/volume build-ubuntu-20.10`
--- build.sh
+++ build.sh
@@ -9,9 +9,10 @@ cd `find . -mindepth 1 -maxdepth 1 -type d`/debian
 cat control | sed 's/^Build-Depends: /Build-Depends: libheif-dev, /' > control.new
 mv control.new control
 
-dch --local tonimelisma 'Add HEIF support'
-# !!! FIX THIS !!!
+dch --local tonimelisma --distribution `lsb_release -cs` 'Add HEIF support'
 
 debuild -S -sd
+
+cd ../..
 
 dput ppa:tonimelisma/ppa `find . -type f -name '*source.changes'`

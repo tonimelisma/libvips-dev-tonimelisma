b088de590c857308ede3b5361a3138ba8f4285d1
Toni Melisma <toni.melisma@iki.fi>
2020-10-15 23:36:33 +0300

    Finalizing build script

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # libvips-dev-tonimelisma
 
 This is my build script to produce libvips (image processing library) Ubuntu packages
-with HEIF support. To use the packages:
+with HEIF support. To install the vips packages:
 
 `sudo add-apt-repository ppa:tonimelisma/ppa`
 
 `sudo apt install libvips42`
 
-Usage
+## Usage for building your own Ubuntu packages
 
-`docker build -f Dockerfile-20.10 -t build-ubuntu-20.10`
+The script and Dockerfile monitor a source package in the given Ubuntu releases,
+and when the source updates, it automatically creates an updated customized package
+and uploads it to your PPA, so your PPA version is always up to date.
 
-`docker run --rm -it -v /tmp/volume:/volume build-ubuntu-20.10`
+If you want to build your own Ubuntu packages with this, you will need to:
+Install docker, set up a PPA at Launchpad and upload your GPG keys,
+make a copy of the Dockerfile and change it to suit your build process. Then run:
+
+`DISTRIBUTION=focal DISTVERSION=20.04 PACKAGE=vips bash build.sh`
+
+`DISTRIBUTION=focal DISTVERSION=20.10 PACKAGE=vips bash build.sh`
+
+You can run this from cron daily (be sure to run it as your user account)
--- build.sh
+++ build.sh
@@ -1,66 +1,68 @@
-#!bash
+#!/bin/bash
 
 PACKAGE=vips
 DATE=`date --iso-8601=minutes`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ -z "${DISTRIBUTION}" ]; then
-	echo Define environment variable DISTRIBUTION (e.g. focal)
+	echo Define environment variable DISTRIBUTION "(e.g. focal)"
 	exit 1
 fi
 
 if [ -z "${DISTVERSION}" ]; then
-	echo Define environment variable DISTVERSION (e.g. 20.04)
+	echo Define environment variable DISTVERSION "(e.g. 20.04)"
 	exit 1
 fi
 
 echo Building base image:
 
-docker build -t build-$PACKAGE:$DISTVERSION -f $DIR/Dockerfile-$DISTVERSION $DIR || echo build failed 1>&2 && exit 1
+docker build -t build-$PACKAGE:$DISTVERSION -f $DIR/Dockerfile-$PACKAGE-$DISTVERSION $DIR || echo build failed 1>&2 && exit 1
 
 echo
 
+if [ ! -d ~/.buildvers ]; then
+	mkdir -p ~/.buildvers
+fi
+
 if [ -f ~/.buildvers/$PACKAGE-$DISTRIBUTION ]; then
 	OLDVERSION=`cat ~/.buildvers/$PACKAGE-DISTRIBUTION`
-	NEWVERSION=`docker run --rm build:20.04 bash -c "apt-get -qq update && rmadison $PACKAGE | grep $DISTRIBUTION | awk {'print $3'}"`
+	NEWVERSION=`docker run --rm build-$PACKAGE:$DISTVERSION bash -c "apt-get -qq update && rmadison $PACKAGE | grep $DISTRIBUTION | awk {'print $3'}"`
 	if [ "$OLDVERSION" = "$NEWVERSION" ]; then
 		echo Versions match, no need to update
 		exit
 	fi
+	echo Version changed from $OLDVERSION to $NEWVERSION, creating new package
 else
 	echo No old version found, creating new package
 fi
 
-echo End
-exit 
+echo -n Press enter to continue or Ctrl-C to abort:' '
+read
 
-COPY GPG KEYS
+clean_gpg() {
+	rm /tmp/volume-$PACKAGE-$DISTRIBUTION/keys.asc
+	exit
+}
 
-apt-get update && rmadison $PACKAGE | grep $DISTRIBUTION | awk {'print $3'}
+trap clean_gpg SIGINT SIGTERM EXIT
 
+mkdir -p /tmp/volume-$PACKAGE-$DISTRIBUTION
+gpg --export-secret-keys --armor >> /tmp/volume-$PACKAGE-$DISTRIBUTION/keys.asc || echo "Couldn't write GPG keys" && exit 1
 
+docker run --rm -i -v /tmp/volume-$PACKAGE-$DISTRIBUTION:/volume build-$PACKAGE-$DISTVERSION bash -s <<EOF
+gpg --import /volume/keys.asc || exit 1
+mkdir -p /tmp/build-$PACKAGE-$DISTRIBUTION-$DATE && cd /tmp/build-$PACKAGE-$DISTRIBUTION-$DATE
+apt-get -q -y source $PACKAGE || exit 1
 
-DOCKER RUN
-set -e
-
-mkdir -p /tmp/build-$DISTRIBUTION-$DATE
-cd /tmp/build-$DATE-$DISTRIBUTION
-
-
-gpg --import /volume/keys.asc
-cd /tmp/build
-apt-get -y source vips
-cd `find . -mindepth 1 -maxdepth 1 -type d`/debian
+cd `find . -mindepth 1 -maxdepth 1 -type d`/debian || exit 1
 
 cat control | sed 's/^Build-Depends: /Build-Depends: libheif-dev, /' > control.new
 mv control.new control
 
-dch --local tonimelisma --distribution `lsb_release -cs` 'Add HEIF support'
-
-debuild -S -sd
-
+dch --local tonimelisma --distribution $DISTRIBUTION 'Add HEIF support' || exit 1
+debuild -S -sd || exit 1
 cd ../..
+dput ppa:$PPANAME/ppa `find . -type f -name '*source.changes'`
+EOF
 
-dput ppa:tonimelisma/ppa `find . -type f -name '*source.changes'`
-
-DELETE GPG KEYS
+gpg_clean

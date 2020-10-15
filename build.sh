878491f0cde546ae0e55fc9970324a9f019a79e5
Toni Melisma <toni.melisma@iki.fi>
2020-10-16 00:22:04 +0300

    I hate escapes

--- build.sh
+++ build.sh
@@ -29,37 +29,46 @@ if [ ! -d ~/.buildvers ]; then
 	mkdir -p ~/.buildvers
 fi
 
+echo Checking newest version...
+NEWVERSION=`docker run --rm build-$PACKAGE:$DISTVERSION bash -c "apt-get -qq update && rmadison $PACKAGE | grep $DISTRIBUTION | sed 's/$PACKAGE . //;s/ . $DISTRIBUTION.*//;s/ //g;'"`
+
 if [ -f ~/.buildvers/$PACKAGE-$DISTRIBUTION ]; then
-	OLDVERSION=`cat ~/.buildvers/$PACKAGE-DISTRIBUTION`
-	NEWVERSION=`docker run --rm build-$PACKAGE:$DISTVERSION bash -c "apt-get -qq update && rmadison $PACKAGE | grep $DISTRIBUTION | awk {'print $3'}"`
+	OLDVERSION=`cat ~/.buildvers/$PACKAGE-$DISTRIBUTION`
 	if [ "$OLDVERSION" = "$NEWVERSION" ]; then
 		echo Versions match, no need to update
 		exit
 	fi
 	echo Version changed from $OLDVERSION to $NEWVERSION, creating new package
 else
-	echo No old version found, creating new package
+	echo No old version found, creating new package for $NEWVERSION
 fi
 
 echo -n Press enter to continue or Ctrl-C to abort:' '
 read
 
 clean_gpg() {
-	rm /tmp/volume-$PACKAGE-$DISTRIBUTION/keys.asc
+	echo Cleaning GPG keys...
+	rm -f /tmp/volume-$PACKAGE-$DISTRIBUTION/keys.asc
+	rm -rf /tmp/volume-$PACKAGE-$DISTRIBUTION
 	exit
 }
 
 trap clean_gpg SIGINT SIGTERM EXIT
 
 mkdir -p /tmp/volume-$PACKAGE-$DISTRIBUTION
-gpg --export-secret-keys --armor >> /tmp/volume-$PACKAGE-$DISTRIBUTION/keys.asc || echo "Couldn't write GPG keys" && exit 1
+gpg --export-secret-keys --armor >> /tmp/volume-$PACKAGE-$DISTRIBUTION/keys.asc || (echo "Couldn't write GPG keys" && exit 1)
 
-docker run --rm -i -v /tmp/volume-$PACKAGE-$DISTRIBUTION:/volume build-$PACKAGE-$DISTVERSION bash -s <<EOF
+docker run --rm -i -v /tmp/volume-$PACKAGE-$DISTRIBUTION:/volume build-$PACKAGE:$DISTVERSION bash -s <<EOF
 gpg --import /volume/keys.asc || exit 1
 mkdir -p /tmp/build-$PACKAGE-$DISTRIBUTION-$DATE && cd /tmp/build-$PACKAGE-$DISTRIBUTION-$DATE
 apt-get -q -y source $PACKAGE || exit 1
 
-cd `find . -mindepth 1 -maxdepth 1 -type d`/debian || exit 1
+
+find . -mindepth 1 -maxdepth 1 -type d
+SRCDIR=\`find . -mindepth 1 -maxdepth 1 -type d\`
+echo What
+echo Source dir is $SRCIDR
+cd $SRCDIR/debian || exit 1
 
 cat control | sed 's/^Build-Depends: /Build-Depends: libheif-dev, /' > control.new
 mv control.new control
@@ -69,7 +78,7 @@ debuild -S -sd || exit 1
 cd ../..
 dput ppa:$PPANAME/ppa `find . -type f -name '*source.changes'`
 EOF
-gpg_clean
+clean_gpg
 
 echo -n Press enter to mark $NEWVERSION as successfully uploaded or Ctrl-C to abort:' '
 read
